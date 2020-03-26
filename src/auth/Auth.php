5219f389afe1682d2a3ef1273ff409e4d9594941
root <root@ip-172-31-22-184.ec2.internal>
2020-03-26 18:07:28 +0000

    auth

--- src/auth/Auth.php
+++ src/auth/Auth.php
@@ -3,6 +3,9 @@
 // Add Composer
 require_once "../../vendor/autoload.php";
 
+// CORS
+require_once "../config/cors.php";
+
 // Add BD Connect
 require_once "../config/conn.php";
 
@@ -21,19 +24,44 @@ if ($f == 'login'){
     echo $Auth->Login();
 }
 
+if ($f == 'check'){
+    $Auth = new Auth();
+    echo $Auth->Check();
+}
+
 
 class Auth
 {
+    public function Check(){
+        global $conn, $post;
+        $token = $post['token'];
+        $key = 'my_secret_key';
+        // eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1ODUyNDIzNDEsImV4cCI6MTU4NTI0NTk0MSwiZGF0YSI6eyJpZCI6MSwibmFtZSI6IkVkdWFyZG8ifX0.E50C4ugU4GTfXNZGxG_P-H_p0i4jstnUsrV4Kn11U_4
+        $data = JWT::decode($token, $key, array('HS256'));
+        
+        // if($data->aud !== null){
+        //     return 'Invalid user logged in.';    
+        // }else{
+        //     return 'valid';
+        // }
+        
+        return json_encode($data);
+        
+        
+    }
+    
     public function Login(){
         global $conn, $post;
 
         // Consulta dato Bd
-        $queryUser = $conn->query("select count(id) cantidad from users")->fetch_array(MYSQLI_ASSOC);
+        $queryUser = $conn->query("select count(id) cantidad from users where email = '{$post['login']['email']}' and
+                                            password = '{$post['login']['password']}'")->fetch_array(MYSQLI_ASSOC);
 
         // Si solo hay un usuario existe
         if ($queryUser['cantidad'] == 1){
             $user = $conn->query("select * from users where email = '{$post['login']['email']}' and
                                             password = '{$post['login']['password']}'")->fetch_array(MYSQLI_ASSOC);
+            // print_r($user);
             $time = time();
             $key = 'my_secret_key';
             $token = array(
@@ -45,14 +73,19 @@ class Auth
                 ]
             );
             $jwt = JWT::encode($token, $key);
+            $auth = 'ok';
+            $error = '';
         }else{
             $jwt = '';
+            $auth = 'no';
+            $error = 'El usuario o contrasenia no son correctos';
         }
 
 
         $array = [
             'token' =>  $jwt,
-            'auth'  =>  'ok'
+            'auth'  =>  $auth,
+            'error' =>  $error
         ];
 
         return json_encode($array);
