f3e6e508a1e35c2e4fe81e919f78b1305b1d6c96
moises <elnaufrago2009@gmail.com>
2020-03-28 12:37:25 -0500

    cors update and readme

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
-# meritocracia
-## api en slin
-Probar comid 
+# meritocracia API
+
+## Instalacion 
+
+Clonar el repositorio
+
+`https://github.com/SocialMeetPeru/meritocracia-api.git`
+
+`cd meritocracia-api/`
+
+Crear una base de datos con el nombre 
+
+`meritocracia_api`
+
+Importar el archivo meritocracia_api.sql a la BD creada
+
+`/src/auth/bd/meritocracia_api.sql`
+
+Poner los datos de acceso de la BD en el archivo 
+
+`/src/auth/config/conn.php`
+
+Ahora si corremos el server en el puerto 8083
+ 
+`php -S localhost:8083`
+
+
--- src/config/cors.php
+++ src/config/cors.php
@@ -1,12 +1,9 @@
 <?php
 
 header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-header("Allow: GET, POST, OPTIONS, PUT, DELETE");
-$method = $_SERVER['REQUEST_METHOD'];
-if($method == "OPTIONS") {
-  die();
-}
+header("Access-Control-Allow-Credentials: true");
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
 
 ?>
